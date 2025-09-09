e89744f4bf86675e831f56b1eea71ac5d51b2e61
enndy <91780685+enndylove@users.noreply.github.com>
2025-09-09 19:29:32 +0300

    404 page

--- client/src/routes/root/index.tsx
+++ client/src/routes/root/index.tsx
@@ -1,9 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { Error } from "@/shared/components/errors/Error";
 import { useAuth } from "@/shared/hooks/useAuth";
-// import { Header } from "@/shared/components/Header";
 import { QueryClient } from "@tanstack/react-query";
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { Link, Outlet, createRootRouteWithContext, useRouter, useCanGoBack } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ThemeProvider } from "@/shared/theme/theme-provider";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,22 +20,43 @@ type MyRouterContext = {
 
 const rootRoute = createRootRouteWithContext<MyRouterContext>()({
   component: RootComponent,
+  notFoundComponent: () => {
+    const router = useRouter()
+    const canGoBack = useCanGoBack()
+
+    return (
+      <Error
+        status={"404"}
+        title={"404 – Page not found"}
+        description="Looks like this page doesn’t exist."
+      >
+        {canGoBack ? (
+          <Button onClick={() => router.history.back()}
+            className="text-primary mt-9 py-3 px-5 text-[1rem]"
+            variant={"ghost"}
+          >
+            Go back
+          </Button>
+        ) : null}
+      </Error>
+    )
+  },
 });
 
 function RootComponent() {
   return (
-    <ThemeProvider>
-      <div className="min-h-svh flex flex-col">
-        {/*<Header />*/}
-        <main className="flex-1 bg-background">
-          {/* This is where child routes will render */}
-          <Outlet />
-        </main>
-        {import.meta.env.MODE === "development" && (
-          <TanStackRouterDevtools position="bottom-left" />
-        )}
-      </div>
-    </ThemeProvider>
+    <div className="min-h-svh overflow-y-hidden max-h-screen pt-16 flex gap-14 flex-col">
+      <Link className="mx-auto" to="/404">
+        <img className="max-h-8" src="/logo.svg" />
+      </Link>
+      <main className="max-w-sm w-full flex-1 bg-background">
+        {/* This is where child routes will render */}
+        <Outlet />
+      </main>
+      {import.meta.env.MODE === "development" && (
+        <TanStackRouterDevtools position="bottom-left" />
+      )}
+    </div>
   );
 }
 
